ac6635af92db2a0bcde5c125658ed669c54cdae2
Thomas Klebel <t.klebel@hotmail.com>
2019-12-27 21:17:29 +0000

    use spaces instead of tabs

--- read_sensor.py
+++ read_sensor.py
@@ -8,11 +8,11 @@ from datetime import datetime
 
 # Parse command line parameters.
 if len(sys.argv) == 2 and (2 <= int(sys.argv[1]) <=10):
-	sleep_duration = int(sys.argv[1])
+    sleep_duration = int(sys.argv[1])
 else:
-	print('Usage: read_sensor.py 2')
-	print('Explanation: read from sensor every two seconds. The second paramter needs to be >=2 & <=10')
-	sys.exit(1)
+    print('Usage: read_sensor.py 2')
+    print('Explanation: read from sensor every two seconds. The second paramter needs to be >=2 & <=10')
+    sys.exit(1)
 
 
 
@@ -21,15 +21,15 @@ sensor = Adafruit_DHT.AM2302
 pin = 4
 
 try:
-	while True:
-		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-		time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-		if humidity is not None and temperature is not None:
-			print('Time={0}  Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(time, temperature, humidity))
-			sleep(sleep_duration)
-		else:
-			print('Failed to get reading. Try again!')
-			sys.exit(1)
+        if humidity is not None and temperature is not None:
+            print('Time={0}  Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(time, temperature, humidity))
+            sleep(sleep_duration)
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
 except KeyboardInterrupt:
-	sys.exit(0)
+    sys.exit(0)
