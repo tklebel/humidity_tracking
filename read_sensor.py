b4a844dbf4876cf8201c6728fd932d212a79ec34
Thomas Klebel <t.klebel@hotmail.com>
2019-12-30 22:34:12 +0000

    write sensor data to db

--- read_sensor.py
+++ read_sensor.py
@@ -4,8 +4,15 @@ import sys
 import Adafruit_DHT
 from time import sleep
 from datetime import datetime
+from functions import connect_db
 
 
+# setup db connection
+con = connect_db()
+cursor = con.cursor()
+
+# set sleep duration for test purposes
+sleep_duration = 30
 
 # define function for printing results
 def print_result(time, humidity, temperature):
@@ -18,15 +25,23 @@ def print_result(time, humidity, temperature):
         print('Failed to get reading. Try again!')
         sys.exit(1)
 
+def write_to_db(cursor, time, humidity, temperature):
+
+    insert_sql = """INSERT INTO humidity_data
+                    VALUES (%s, %s, %s);"""
+
+    cursor.execute(insert_sql, (time, humidity, temperature))
+
+    con.commit()
 
 
-# Parse command line parameters.
-if len(sys.argv) == 2 and (2 <= int(sys.argv[1]) <=10):
-    sleep_duration = int(sys.argv[1])
-else:
-    print('Usage: read_sensor.py 2')
-    print('Explanation: read from sensor every two seconds. The second paramter needs to be >=2 & <=10')
-    sys.exit(1)
+# # Parse command line parameters.
+# if len(sys.argv) == 2 and (2 <= int(sys.argv[1]) <=10):
+#     sleep_duration = int(sys.argv[1])
+# else:
+#     print('Usage: read_sensor.py 2')
+#     print('Explanation: read from sensor every two seconds. The second paramter needs to be >=2 & <=10')
+#     sys.exit(1)
 
 
 
@@ -39,7 +54,8 @@ try:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
         time = datetime.now()
 
-        print_result(time, humidity, temperature)
+        #print_result(time, humidity, temperature)
+        write_to_db(cursor, time, humidity, temperature)
 
         sleep(sleep_duration)
 except KeyboardInterrupt:
