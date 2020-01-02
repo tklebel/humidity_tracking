c1c37177ef539b7f0d86602d60af0707f083b2de
Thomas Klebel <t.klebel@hotmail.com>
2020-01-02 00:14:38 +0000

    move printing to functions file for reuse

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import psycopg2
+import Adafruit_DHT
 
 
 def connect_db():
@@ -8,3 +9,14 @@ def connect_db():
     connection = psycopg2.connect(params)
     
     return(connection)
+
+# define function for printing results
+def format_result(time, humidity, temperature):
+    if humidity is not None and temperature is not None:
+        # format time string
+        time_pretty = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        return('Time={0}  Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(time_pretty, temperature, humidity))
+    else:
+        return('Failed to get reading. Try again!')
+        sys.exit(1)
--- read_sensor.py
+++ read_sensor.py
@@ -18,17 +18,6 @@ sleep_duration = 30
 sensor = Adafruit_DHT.AM2302
 pin = 4
 
-# define function for printing results
-def print_result(time, humidity, temperature):
-    if humidity is not None and temperature is not None:
-        # format time string
-        time_pretty = time.strftime('%Y-%m-%d %H:%M:%S')
-
-        print('Time={0}  Temp={1:0.1f}*C  Humidity={2:0.1f}%'.format(time_pretty, temperature, humidity))
-    else:
-        print('Failed to get reading. Try again!')
-        sys.exit(1)
-
 # function for writing results to database
 def write_to_db(cursor, time, humidity, temperature):
 
