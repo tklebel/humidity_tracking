da12d49251b4bfc6e394278dc910430348b3e278
Thomas Klebel <t.klebel@hotmail.com>
2020-01-03 21:29:25 +0000

    display current humidity on lcd-display

--- read_sensor.py
+++ read_sensor.py
@@ -5,6 +5,9 @@ import Adafruit_DHT
 from time import sleep
 from datetime import datetime
 from functions import connect_db
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+
 
 
 # setup db connection
@@ -31,7 +34,30 @@ def write_to_db(cursor, time, humidity, temperature):
 def cleanup_db():
     con.close()
     cursor.close()
-    print("Database connections closed")
+    print("Database connections closed.")
+
+def clear_display():
+    lcd.clear()
+    print("Display cleared.")
+
+
+## setup display stuff (copied from example from freenove) #########
+PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+try:
+    mcp = PCF8574_GPIO(PCF8574_address)
+except:
+    try:
+        mcp = PCF8574_GPIO(PCF8574A_address)
+    except:
+        print ('I2C Address Error !')
+        exit(1)
+# Create LCD, passing in MCP GPIO adapter.
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+mcp.output(3,1)     # turn on LCD backlight (set second param to 0 for no backlight)
+lcd.begin(16,2)     # set number of LCD lines and columns
+lcd.setCursor(0,0)  # set cursor position
 
 def main():
     print("Reading data and writing to database every", sleep_duration, "seconds.")
@@ -40,6 +66,11 @@ def main():
         time = datetime.now()
 
         write_to_db(cursor, time, humidity, temperature)
+
+        # display measurements on display
+
+        lcd.message('Temp={0:0.1f}*C\n'.format(temperature))
+        lcd.message('Humidity={0:0.1f}%'.format(humidity))
 
         sleep(sleep_duration)
 
@@ -50,5 +81,7 @@ if __name__=='__main__':
     except KeyboardInterrupt:
         sys.exit(0)
     finally:
+        print('\nTerminating...')
         cleanup_db()
+        clear_display()
 
