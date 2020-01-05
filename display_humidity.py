c01171bd4b2c9e2dafdb7bdd6267cf2a2cab71ed
Thomas Klebel <t.klebel@hotmail.com>
2020-01-05 10:37:07 +0000

    extract code for LCD into separate script

---
+++ display_humidity.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+import Adafruit_DHT
+import logging
+from time import sleep
+from functions import get_data
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+
+
+
+# set sleep duration for test purposes
+sleep_duration = 30
+
+# set up logging for debugging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def clear_display():
+    lcd.clear()
+    print("Display cleared.")
+
+
+## setup display stuff (copied from example from freenove) #########
+PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+try:
+    mcp = PCF8574_GPIO(PCF8574_address)
+except:
+    try:
+        mcp = PCF8574_GPIO(PCF8574A_address)
+    except:
+        print ('I2C Address Error !')
+        exit(1)
+# Create LCD, passing in MCP GPIO adapter.
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+mcp.output(3,1)     # turn on LCD backlight (set second param to 0 for no backlight)
+lcd.begin(16,2)     # set number of LCD lines and columns
+lcd.clear()         # clear the display in case something was not removed before
+
+
+def main():
+    while True:
+        time, humidity, temperature = get_data()
+
+        if humidity is None or temperature is None:
+            # this part seems not to be working properly
+            logger.info("Readings could not be recovered via recursion")
+            sleep(sleep_duration)
+            continue # do not update the display this time
+
+        # display measurements on display
+        lcd.setCursor(0,0)  # set cursor position. this needs to be here, otherwise the display keeps old output
+        lcd.message('Temp={0:0.1f}*C\n'.format(temperature))
+        lcd.message('Humidity={0:0.1f}%'.format(humidity))
+
+        sleep(sleep_duration)
+
+
+if __name__=='__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    finally:
+        print('\nTerminating...')
+        clear_display()
+
--- read_sensor.py
+++ read_sensor.py
@@ -5,9 +5,6 @@ import Adafruit_DHT
 import logging
 from time import sleep
 from functions import connect_db, get_data
-from PCF8574 import PCF8574_GPIO
-from Adafruit_LCD1602 import Adafruit_CharLCD
-
 
 
 # setup db connection
@@ -38,46 +35,12 @@ def cleanup_db():
     cursor.close()
     print("Database connections closed.")
 
-def clear_display():
-    lcd.clear()
-    print("Display cleared.")
-
-
-## setup display stuff (copied from example from freenove) #########
-PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
-PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
-# Create PCF8574 GPIO adapter.
-try:
-    mcp = PCF8574_GPIO(PCF8574_address)
-except:
-    try:
-        mcp = PCF8574_GPIO(PCF8574A_address)
-    except:
-        print ('I2C Address Error !')
-        exit(1)
-# Create LCD, passing in MCP GPIO adapter.
-lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
-mcp.output(3,1)     # turn on LCD backlight (set second param to 0 for no backlight)
-lcd.begin(16,2)     # set number of LCD lines and columns
-lcd.clear()         # clear the display in case something was not removed before
-
-
 def main():
     print("Reading data and writing to database every", sleep_duration, "seconds.")
     while True:
         time, humidity, temperature = get_data()
 
         write_to_db(cursor, time, humidity, temperature)
-
-        if humidity is None or temperature is None:
-            logger.info("Readings could not be recovered via recursion")
-            sleep(sleep_duration)
-            continue # do not update the display this time
-
-        # display measurements on display
-        lcd.setCursor(0,0)  # set cursor position. this needs to be here, otherwise the display keeps old output
-        lcd.message('Temp={0:0.1f}*C\n'.format(temperature))
-        lcd.message('Humidity={0:0.1f}%'.format(humidity))
 
         sleep(sleep_duration)
 
@@ -90,5 +53,4 @@ if __name__=='__main__':
     finally:
         print('\nTerminating...')
         cleanup_db()
-        clear_display()
 
