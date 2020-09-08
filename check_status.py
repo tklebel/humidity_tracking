66f3680f6d9e1f881709cc3f822e4ec66b7670c5
Thomas Klebel <t.klebel@hotmail.com>
2020-09-08 21:58:31 +0100

    draft script for checking status of processes

---
+++ check_status.py
@@ -0,0 +1,19 @@
+# This file checks whether all processes are running and alerts otherwise
+
+import subprocess
+import re  
+
+result = subprocess.run('ps aux | grep python', shell=True, stdout=subprocess.PIPE)
+result_decoded = result.stdout.decode('utf-8')
+
+# print(result_decoded)
+
+def check_status(filename):
+	res = re.findall(filename, result_decoded)
+
+	if res:
+		print(filename + ' is running.')
+
+
+for file in ['read_sensor.py', 'bot.py', 'display_humidity.py']:
+	check_status(file)
