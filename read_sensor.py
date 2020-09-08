d2a17fdd59e66a7c7fcabc0b668141141e0f81f4
Thomas Klebel <t.klebel@hotmail.com>
2020-09-08 21:34:00 +0100

    close cursors first

--- read_sensor.py
+++ read_sensor.py
@@ -6,21 +6,19 @@ import logging
 from time import sleep
 from functions import connect_db, get_data, create_logger
 
-
 # setup db connection
 con = connect_db()
 cursor = con.cursor()
 
-# set sleep duration for test purposes
-sleep_duration = 30
+sleep_duration = 60
 
 # set up logging for debugging
 logger = create_logger('read_sensor.log')
 
 
+
 # function for writing results to database
 def write_to_db(cursor, time, humidity, temperature):
-
     insert_sql = """INSERT INTO humidity_data
                     VALUES (%s, %s, %s);"""
 
@@ -29,12 +27,13 @@ def write_to_db(cursor, time, humidity, temperature):
     con.commit()
 
 def cleanup_db():
-    con.close()
     cursor.close()
+    con.close()
     logger.info("Database connections closed.")
 
 def main():
     logger.info(f'Reading data and writing to database every {sleep_duration} seconds.')
+
     while True:
         time, humidity, temperature = get_data()
 
